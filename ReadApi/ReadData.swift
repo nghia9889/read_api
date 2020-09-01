d29e37ae1515d52fa479870adcafb5a71ddeb6cb
Tao Trong Nghia <nghia.taotrong@V000123s-Mac-mini.local>
2020-09-01 13:49:55 +0700

    vv

---
+++ ReadApi/ReadData.swift
@@ -0,0 +1,17 @@
+//
+//  Article.swift
+//  ReadApi
+//
+//  Created by Tao Trong Nghia on 8/31/20.
+//  Copyright © 2020 Tao Trong Nghia. All rights reserved.
+//
+
+import UIKit
+
+class Article: NSObject {
+    var user: String?
+    var age: String?
+    var gender: String?
+    var location: String?
+    var imageUrl: String?
+}
